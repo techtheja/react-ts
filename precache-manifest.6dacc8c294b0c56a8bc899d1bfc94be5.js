ecd95b3a11d44052d54b247f9b104fca9d3f80e3
Thejaswi A <thejaswia71@gmail.com>
2020-07-22 19:09:40 +0530

    Updates

---
+++ precache-manifest.6dacc8c294b0c56a8bc899d1bfc94be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcdb452af797c2a4d24d98fd6158751",
+    "url": "/react-ts/index.html"
+  },
+  {
+    "revision": "496b4a23ffeca8afd9ab",
+    "url": "/react-ts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d02d7bff1345ddcb57e6",
+    "url": "/react-ts/static/js/2.6f1c3b94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ts/static/js/2.6f1c3b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496b4a23ffeca8afd9ab",
+    "url": "/react-ts/static/js/main.41394004.chunk.js"
+  },
+  {
+    "revision": "a370ce40761f9886c935",
+    "url": "/react-ts/static/js/runtime-main.1d70cea5.js"
+  }
+]);
